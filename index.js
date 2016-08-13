415e233ccf57f1cdb44d8b61ddab28ef5d6adc52
Bernardo <bernamariano@gmail.com>
2016-08-13 16:41:45 -0300

    default port 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const server = require('./src/server.js')
 
 
-let port = process.argv[2]
+let port = process.argv[2] || 3000
 
 server.listen(port, () => {
     console.log(`Running on port ${ port }...`)
